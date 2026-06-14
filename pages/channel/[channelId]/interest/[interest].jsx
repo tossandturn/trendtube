92cd538450ee15e6173bd9c77eea79af53c7876c
TrendTube Dev <dev@trendtube.app>
2026-06-15 00:09:01 +0800

    fix: correct import path for youtube-analytics-engine

--- pages/channel/[channelId]/interest/[interest].jsx
+++ pages/channel/[channelId]/interest/[interest].jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { analyzeVideo, analyzeVideoSEO } from '../../../lib/youtube-analytics-engine';
+import { analyzeVideo, analyzeVideoSEO } from '../../../../lib/youtube-analytics-engine';
 
 /**
  * Channel Interest Filter Page
